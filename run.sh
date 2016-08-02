3edaa4aef7203c2e146d21c39317798f6f2f684d
Jake Varley <jakevarley@gmail.com>
2016-08-02 14:50:38 -0400

    adding example run script

---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+~/ros/completion_comparison_ws/devel/lib/mirror_objects/mirrorAndReconstruct /home/jvarley/shape_completion_data2_aligned/rubbermaid_ice_guard_pitcher_blue/1_1_17/c_partial_of.pcd -p 0 -100 0 5 /home/jvarley/out -f ../../../src/mirror_objects/demo/calib.txt 
